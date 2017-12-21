3fb21095efe07fd34f06e607d45b643306a25ef5
MONTEL ALICE p1309529 <alice.montel@etu.univ-lyon1.fr>
2017-12-21 11:31:51 +0100

    un début joli

--- docs/scripts/comparison.js
+++
@@ -1,6 +0,0 @@
-function displayComparison(d, i){
-        stringHtml=' <div class="row"> <div class="card deep-orange lighten-1"><div class="card-content white-text">';
-        stringHtml+=' <span class="card-title" id="title-card">COMPARISONS</span>';
-        stringHtml+='<p id="text-card"><div id="svgcomparision">des graphes pertinents </div></p></div></div></div>';
-        document.getElementById("comparison").innerHTML=stringHtml;
-}
